2799600e004c9c2b7f4ffd916fdebab8b1323e98
Nihilkri <nehilkori@gmail.com>
2014-08-13 12:18:33 -0500

    Initialize

---
+++ Stars/StarDef.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stars {
+	/// <summary>
+	/// Class Definition for the stars
+	/// </summary>
+	class StarDef {
+		#region Variables
+		String name;
+		int id, x, y;
+		/// <summary>
+		/// Gravity, Temperature, Radiation
+		/// </summary>
+		int G, T, R;
+		/// <summary>
+		/// Surface Ironium, Boranium, Germanium
+		/// </summary>
+		int sI, sB, sG;
+		/// <summary>
+		/// Concentrations of Ironium, Boranium, Germanium
+		/// </summary>
+		int cI, cB, cG;
+
+
+		#endregion Variables
+
+	}
+}

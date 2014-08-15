6940566a2fbd0ed030d9bf246e591fdb88620ca5
Nihilkri <nehilkori@gmail.com>
2014-08-14 21:39:09 -0500

    Testing menu selection styles

--- Planets/frmMain.cs
+++ Planets/frmMain.cs
@@ -52,7 +52,8 @@ namespace Planets {
 
 		#endregion Program
 		#region Graphics
-		public static Bitmap TitleBackground = new Bitmap(@"\\NIHILKRI\Shared\Alien-Planet-In-Nature.jpg");
+		public static Bitmap TitleBackground = new Bitmap(@"\\NIHILKRI\Shared\Alien-Planet.png"); //-In-Nature.jpg
+		public static Bitmap TitleBackground2 = new Bitmap(@"\\NIHILKRI\Shared\Alien-Planet-In-Nature.jpg"); //
 
 		#endregion Graphics
 
@@ -132,23 +133,41 @@ namespace Planets {
 
 			ft = DateTime.Now - st;
 			gb.DrawString(ft.TotalMilliseconds.ToString() + "ms", Font, Brushes.White, 0, 0);
-			gb.DrawString((1000 / ft.TotalMilliseconds).ToString() + " FPS", Font, Brushes.White, 0, 16);
+			gb.DrawString(ft.TotalMilliseconds == 0 ? "Infinite" : (1000 / (int)ft.TotalMilliseconds).ToString() + " FPS", Font, Brushes.White, 0, 16);
+			gb.DrawString(tfr.ToString(), Font, Brushes.White, 0, 32);
 			gf.DrawImage(gi, 0, 0);
 		}
 		private Point mp = new Point();
 		private Font titlefont = new Font("Razer Header Regular", 72, FontStyle.Italic);
 		private Font menufont = new Font("Razer Header Regular", 18, FontStyle.Italic);
-		private Button newgame;
+		int oa = 250, oc = 250;
 		private void DrawTitle() {
-			gb.DrawImage(TitleBackground, 0, 0, fx, fy);
-
+			if(tfr == 1) gb.DrawImage(TitleBackground, 0, 0, fx, fy); else gb.FillRectangle(Brushes.Black, 0, 0, 64, 48);
+			//if(tfr % 20 > 10) {
+			//	gb.DrawImage(tfr % 10 > 5 ? TitleBackground : TitleBackground2, 0, 0, fx, fy); 
+			//} else {
+			//	gb.DrawImage(TitleBackground, new Rectangle(0, 0, fx2, fy), new Rectangle(0, 0, TitleBackground.Width / 2, TitleBackground.Height), GraphicsUnit.Pixel);
+			//	gb.DrawImage(TitleBackground2, new Rectangle(fx2 - 1, 0, fx2, fy), new Rectangle(TitleBackground2.Width / 2 - 1, 0, TitleBackground2.Width / 2, TitleBackground2.Height), GraphicsUnit.Pixel);
+			//}
 			//gb.DrawString("Planets!", new Font("Razer Header Regular", 73, FontStyle.Italic), Brushes.Red, 828, 178);
-			//gb.DrawString("Planets!", titlefont, Brushes.White, 1009, 48);//mp);//830, 180);
-			gb.DrawString("Planets!", titlefont, Brushes.White, mp);//830, 180);
-			tim.Interval = 250;
+			gb.DrawString("Planets!", titlefont, Brushes.White, 1009, 48);//mp);//830, 180);
+			//gb.DrawString("Planets!", titlefont, Brushes.White, mp);//830, 180);
+			//tim.Interval = 250;
 			//newgame = new Button() { Left = 50, Top = 800 }; newgame.Show();
-			gb.DrawString("New Game   " + new string(' ', (int)(4-(tfr % 4))) + " < < <", menufont, Brushes.Gray, 50, 800);
-			gb.DrawString("Load Game", menufont, Brushes.Gray, 50, 825);
+			//gb.DrawString("New Game   " + new string(' ', (int)(4 - (tfr % 4))) + " < < <", menufont, Brushes.Gray, 50, 800);
+			int na = 255 - 15 * (16 - (Math.Abs((int)(tfr % 32 - 16))));
+			int nc = (int)(127 + 128 * Math.Sin(tfr / 20.0));
+			Color a = Color.FromArgb(na, 255, 255, 255);
+			Color c = Color.FromArgb(nc, 255, 255, 255);
+			Pen pa = new Pen(a), pc = new Pen(c);
+			gb.DrawLine(pa, 249 + (int)(tfr % 500), 700 + oa, 250 + (int)(tfr % 500), 700 + na);
+			gb.DrawLine(pc, 249 + (int)(tfr % 500), 500 + oc, 250 + (int)(tfr % 500), 500 + nc);
+
+			SolidBrush b = new SolidBrush(c);
+			SolidBrush d = new SolidBrush(a);
+			gb.FillRectangle(Brushes.Black, 50, 800, 200, 100);
+			gb.DrawString("New Game", menufont, b, 50, 800);
+			gb.DrawString("Load Game", menufont, d, 50, 825);
 			gb.DrawString("Options", menufont, Brushes.Gray, 50, 850);
 			gb.DrawString("Quit", menufont, Brushes.Gray, 50, 875);
 
